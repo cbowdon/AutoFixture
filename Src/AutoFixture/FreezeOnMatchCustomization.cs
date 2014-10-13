c83c1a88ad4dbdad9852db322aff3f968353cc66
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-10-13 21:54:51 +0200

    Adds FreezeOnMatchCustomization for #120.
    This customization uses request specifications
    to freeze specimens according to a finite set of criteria.
    These criteria can be based on matching type variance,
    member names or a combination of both.

---
+++ Src/AutoFixture/FreezeOnMatchCustomization.cs
@@ -0,0 +1,59 @@
+using System;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Ploeh.AutoFixture
+{
+    public class FreezeOnMatchCustomization : ICustomization
+    {
+        private readonly Type targetType;
+        private readonly IRequestSpecification matcher;
+        private IFixture fixture;
+
+        public FreezeOnMatchCustomization(Type targetType)
+            : this(targetType, new ExactTypeSpecification(targetType))
+        {
+        }
+
+        public FreezeOnMatchCustomization(
+            Type targetType,
+            IRequestSpecification matcher)
+        {
+            this.targetType = targetType;
+            this.matcher = matcher;
+        }
+
+        public Type TargetType
+        {
+            get { return this.targetType; }
+        }
+
+        public IRequestSpecification Matcher
+        {
+            get { return this.matcher; }
+        }
+
+        public void Customize(IFixture fixture)
+        {
+            Require.IsNotNull(fixture, "fixture");
+
+            this.fixture = fixture;
+
+            FreezeTypeForMatchingRequests();
+        }
+
+        private void FreezeTypeForMatchingRequests()
+        {
+            this.fixture.Customizations.Add(
+                new FilteringSpecimenBuilder(
+                    FreezeTargetType(),
+                    this.matcher));
+        }
+
+        private ISpecimenBuilder FreezeTargetType()
+        {
+            var context = new SpecimenContext(this.fixture);
+            var specimen = context.Resolve(this.targetType);
+            return new FixedBuilder(specimen);
+        }
+    }
+}
